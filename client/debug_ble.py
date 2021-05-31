3a91910bed9cce3c114ffceb858df453a50b55cb
Bennett <hello@vantezzen.io>
2021-05-31 20:31:04 +0200

    Create debug_ble.py

---
+++ client/debug_ble.py
@@ -0,0 +1,23 @@
+"""
+CWAInfo CWA BLE Debugging
+Constantly log number of device in proximity
+~~~~~~~~~~~~~~~~~~~
+:copyright: (c) 2021-present Bennett Hollstein
+:license: MIT, see LICENSE for more details.
+"""
+import os
+import configparser
+from collector import Collector
+
+project_dir = os.path.dirname(__file__)
+if len(project_dir) != 0:
+    project_dir += "/"
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+collector = Collector()
+
+while True:
+  devices = collector.scan()
+  print("Devices: " + str(devices))
